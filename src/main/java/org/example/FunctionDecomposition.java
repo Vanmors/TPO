591ebf5b3305e6b8805bfe0668db0fbb9957994c
User <ilya31zagor03@mail.ru>
2024-03-02 19:46:01 +0300

    Made decomposeFunction method

--- src/main/java/org/example/FunctionDecomposition.java
+++ src/main/java/org/example/FunctionDecomposition.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class FunctionDecomposition {
-    public static double decompose(int precision, int value){
-        return 0.0;
+    public static double decompose(int precision, double value){
+        if (precision <= 0) throw new RuntimeException();
+        if (precision == 1){
+            return value;
+        }
+        if (precision == 2){
+            return value + Math.pow(value, 3) / 6d;
+        }
+
+        double result = value + Math.pow(value, 3) / 6d;
+        long lastOdd = 1;
+        double lastCoefficient = 1d / 2d;
+        for (int i = 2; i < precision; i++){
+            lastOdd += 2;
+            result += lastCoefficient * (lastOdd) * Math.pow(value, (lastOdd + 2)) / ((lastOdd + 1) * (lastOdd + 2));
+        }
+        return result;
     }
 }
