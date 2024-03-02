b76a1e60bd50b28eb99d6a2070a3f5f55e120f8f
ivanmorikov <ivanmorikov48@gmail.com>
2024-03-02 19:30:16 +0300

    Parameterized test for decompose method has been added

--- src/test/java/FunctionDecompositionTest.java
+++ src/test/java/FunctionDecompositionTest.java
@@ -1,2 +1,27 @@
+import org.example.FunctionDecomposition;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FunctionDecompositionTest {
+
+
+    static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 1, 2, 2 },
+                { 2, 3, 6 },
+                { 5, 7, 35 },
+                { 10, 5, 50 }
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void decomposeTest(int a, int b, int expected) {
+        assertEquals(expected, FunctionDecomposition.decompose(a, b), "Сумма некорректна");
+    }
 }
